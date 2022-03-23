610e682cc8ad501ea54504936b49d0d859181966
ezechinemere1 <ezechinemere1@gmail.com>
2022-03-23 14:38:42 +0100

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,22 +8,35 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'Samsung galaxy A21',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'Technology : GSM / CDMA / HSPA/ EVDO / LTE. Announced :2020, April 08'
   },
   {
     id: 2,
-    name: 'Phone Mini',
+    name: 'Gionee S11',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'Technology : GSM / CDMA / HSPA / LTE. Announced:2017, November. Released 2017, December '
   },
   {
     id: 3,
-    name: 'Phone Standard',
+    name: 'Nokia T20',
     price: 299,
-    description: ''
-  }
+    description: 'Power through with a 8200 mAh battery and maximize your view with a 10.4" 2K display.'
+  },
+  {
+    id: 4,
+    name: 'iphone 13',
+    price: 299,
+    description: 'The iPhone 13 and iPhone 13 Mini are smartphones designed, developed, marketed, and sold by Apple Inc.'
+  },
+  {
+    id: 5,
+    name: 'Xiaomi Mi 11X Pro',
+    price: 299,
+    description: 'Technology: GSM / HSPA / LTE / 5G. Announced :	2021, April 23'
+  },
+
 ];
 
 
